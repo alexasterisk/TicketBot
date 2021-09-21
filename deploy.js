32823f0fe9b216af68e4fc288f185f8ec50f2ae2
Warfurret <31458482+warfurret@users.noreply.github.com>
2021-09-21 00:35:06 -0400

    fixed "update" module

--- deploy.js
+++ deploy.js
@@ -1,32 +1,43 @@
-const { guildId } = require('./config.json');
-const { readdirSync } = require('fs');
+const { readdirSync } = require('fs')
+const { guildId } = require('./config.json')
 
 module.exports = {
-    async run (client) {
-        const guild = client.guilds.cache.get(guildId);
-        let commands = guild?.commands || client.application?.commands;
+    async deployAll (client) {
+        const guild = client.guilds.cache.get(guildId)
+        let commands = guild?.commands
 
-        const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
+        // Creating a table for setting the permissions for each command later
+        let fullPermissions = []
+
+        // Reading through each command file to state if it needs created or edited, most importantly updating the permissions necessary
+        // The reason the commands aren't just recreated each time is to not spam the Discord API, I don't wany my bot taken down afterall
+
+        const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'))
         for (const file of commandFiles) {
-            const command = require(`./commands/${file}`);
+            const command = require(`./commands/${file}`)
 
-            if (!command.isPushed) {
-                commands?.create({
-                    name: command.name,
-                    description: command.description,
-                    options: command.options
-                });
-            } else if (command.needsUpdated) {
-                const cmd = client.application?.commands.cache.get(c => c.name === command.name);
+            // Creating a command
+            // This will trigger only if the command specifically says it needs "pushed" AND the command doesn't already exist
 
-                if (cmd) {
-                    cmd.edit({
-                        name: command.name,
-                        description: command.description,
-                        options: command.options
-                    });
-                };
-            };
-        };
+            const cmd = await commands?.create({
+                name: command.name,
+                description: command.description,
+                options: command.options,
+                defaultPermission: command.permissionForAll || true
+            })
+
+            console.log(`Created ${command.name}!`)
+
+            // Updating all of the permissions for each command
+            // Do not forget defaultPermission when creating/editing a command
+
+            fullPermissions.push({
+                id: cmd.id,
+                permissions: command.permissions || []
+            })
+        }
+
+        await guild?.commands.permissions.set({ fullPermissions })
+        console.log(`Updated all permissions!`)
     }
-};
+}
